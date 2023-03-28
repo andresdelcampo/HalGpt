54421a596fa5a07c777da4b9cec634a3b5e694e0
andresdelcampo <33843515+andresdelcampo@users.noreply.github.com>
2023-03-28 20:55:43 +0200

    Preparing first upload version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GPT-UI")]
+[assembly: AssemblyTitle("HalGpt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GPT-UI")]
+[assembly: AssemblyProduct("HalGpt")]
 [assembly: AssemblyCopyright("Copyright © Andrés del Campo Novales, 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
